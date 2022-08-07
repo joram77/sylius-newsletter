2cb930e43b8279210760c53e14667ff9585ae4bc
wethinkonline <info@wethinkonline.be>
2022-08-07 08:42:59 +0200

    Fix NewsletterSendService validate if newsletter id exists

--- JoramSyliusNewsletterPlugin/src/Service/NewsletterSendService.php
+++ JoramSyliusNewsletterPlugin/src/Service/NewsletterSendService.php
@@ -48,42 +48,45 @@ class NewsletterSendService
     public function send($newsletterId, OutputInterface $output): void
     {
         $newsletter = $this->newsletterRepository->find($newsletterId);
-        $numSent = 0;
-        $numSubscribers = $newsletter->getSubscribers()->count();
-        echo "Sending newsletter {$newsletter->getId()} {$newsletter->getSubject()}...\n";
-        $i = 0;
-        $errors = '';
-        try {
-            $template = $this->templating->createTemplate(nl2br($newsletter->getContent()));
-
-        } catch (LoaderError|SyntaxError $e) {
-            echo "Error parsing newsletter " . $e->getMessage();
-            return;
-        }
-        foreach ($newsletter->getSubscribers() as $subscriber) {
-            $parsedContent = $template->render(array('first_name' => $subscriber->getFirstname(), 'newsletter_name' => $newsletter->getSubject()));
-            if ($output != null && $i !== 0) {
-                NewsletterSendCommand::clearLine($output);
-            }
-            $i++;
-            echo 'Sent to subscriber ' . $i . ' of ' . $numSubscribers . "\n";
-            $mail = (new Email())->from('no-reply@netenders.com');
-            $mail->to($subscriber->getUser()->getEmail())
-                ->subject($newsletter->getSubject())
-                ->html($parsedContent);
+        if ($newsletter == null) {
+            echo "Newsletter with id {$newsletterId} does not exist\n";
+        } else {
+            $numSent = 0;
+            $numSubscribers = $newsletter->getSubscribers()->count();
+            echo "Sending newsletter {$newsletter->getId()} {$newsletter->getSubject()}...\n";
+            $i = 0;
+            $errors = '';
             try {
-                $this->mailer->send($mail);
-                $numSent += 1;
-            } catch (TransportExceptionInterface $e) {
-                $errors .= "Error sending to 1 recipient " . $e->getMessage() . "\n";
+                $template = $this->templating->createTemplate(nl2br($newsletter->getContent()));
+            } catch (LoaderError|SyntaxError $e) {
+                echo "Error parsing newsletter " . $e->getMessage();
+                return;
             }
-            if ($i % self::FREE_SERVICES_MEMORY_NUM_RECORDS == 0) {
-                $this->entityManager->clear();
-                $this->messageLoggerListener->reset();
-                /*$this->registry->reset();*/
+            foreach ($newsletter->getSubscribers() as $subscriber) {
+                $parsedContent = $template->render(array('first_name' => $subscriber->getFirstname(),
+                    'newsletter_name' => $newsletter->getSubject()));
+                if ($output != null && $i !== 0) {
+                    NewsletterSendCommand::clearLine($output);
+                }
+                $i++;
+                echo 'Sent to subscriber ' . $i . ' of ' . $numSubscribers . "\n";
+                $mail = (new Email())->from('no-reply@netenders.com');
+                $mail->to($subscriber->getUser()->getEmail())
+                    ->subject($newsletter->getSubject())
+                    ->html($parsedContent);
+                try {
+                    $this->mailer->send($mail);
+                    $numSent += 1;
+                } catch (TransportExceptionInterface $e) {
+                    $errors .= "Error sending to 1 recipient " . $e->getMessage() . "\n";
+                }
+                if ($i % self::FREE_SERVICES_MEMORY_NUM_RECORDS == 0) {
+                    $this->entityManager->clear();
+                    $this->messageLoggerListener->reset();
+                }
             }
+            echo 'Done sending ' . $numSent . " messages.\n";
+            if ($errors != '') echo "Error sending messages: $errors\n";
         }
-        echo 'Done sending ' . $numSent . " messages.\n";
-        if ($errors != '') echo "Error sending messages: $errors\n";
     }
 }
